a63dfc2edad9ac08f2ca00d1cb8677b4a877dc16
Noeline Marie Jeanne <noeline.mar@onja.org>
2021-01-30 09:42:06 +0300

    Use date dependencies because that is cleaner and shorter

--- src/container/jobs.js
+++ src/container/jobs.js
@@ -1,44 +1,43 @@
 import React, { useContext } from 'react'
+import { formatDistance } from 'date-fns';
+
 import { Card, Loading } from '../components'
 import { GlobalContext } from '../context/GlobalContext'
 
 export default function JobContainer() {
     const { state } = useContext(GlobalContext);
     const { jobs } = state;
+
+    const formattedDate = date => {
+        return formatDistance(new Date(date), new Date());
+    };
+    
     return (
         <>
             <Loading>Loading...</Loading>
             {jobs && 
                 <Card>
-                    {jobs.map((job) => {
-                        const options = { year: "numeric", month: "numeric", day: "numeric" };
-                        const today = new Date().toLocaleString("en-US", options);
-                        const createdAt = new Date(job.created_at).toLocaleString("en-US", options);
-                        const totalDate = new Date(today) - new Date(createdAt)
-                        const daysAgo = Math.floor( totalDate / (1000 * 3600 * 24));
-
-                        return ( 
-                            <Card.Item key={job.id}>
-                                {job.company_logo === null 
-                                    ? <Card.LogoNotFound>not found</Card.LogoNotFound> 
-                                    : <Card.Logo src={job.company_logo} alt={`${job.title} at ${job.company}`} />
-                                }
-                                <Card.Feature>
-                                    <Card.Subtitile>{job.company}</Card.Subtitile>
-                                    <Card.Title>{job.title}</Card.Title>
-                                    {job.type && <Card.CustomLabel>{job.type}</Card.CustomLabel>}
-                                    <Card.Meta>
-                                        <Card.Location>
-                                            <span>{job.location}</span>
-                                        </Card.Location>
-                                        <Card.Date dateTime={job.created_at}>
-                                            {daysAgo} {daysAgo > 1 ? "days" : "day"}
-                                        </Card.Date>
-                                    </Card.Meta>
-                                </Card.Feature>
-                            </Card.Item>
-                        )
-                    })}
+                    {jobs.map((job) => (
+                        <Card.Item key={job.id}>
+                            {job.company_logo === null 
+                                ? <Card.LogoNotFound>not found</Card.LogoNotFound> 
+                                : <Card.Logo src={job.company_logo} alt={`${job.title} at ${job.company}`} />
+                            }
+                            <Card.Feature>
+                                <Card.Subtitile>{job.company}</Card.Subtitile>
+                                <Card.Title>{job.title}</Card.Title>
+                                {job.type && <Card.CustomLabel>{job.type}</Card.CustomLabel>}
+                                <Card.Meta>
+                                    <Card.Location>
+                                        <span>{job.location}</span>
+                                    </Card.Location>
+                                    <Card.Date dateTime={job.created_at}>
+                                        {formattedDate(job.created_at)}
+                                    </Card.Date>
+                                </Card.Meta>
+                            </Card.Feature>
+                        </Card.Item>
+                    ))}
                 </Card>
             }
         </>
